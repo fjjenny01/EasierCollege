22088ecd1e99762a07883c09ed0c4c874c84f0b1
jinfang <fangjx@dukes.jmu.edu>
2015-02-20 01:44:21 -0500

    second commit

--- app/src/main/java/com/example/jinfang/easiercollege/Splash.java
+++ app/src/main/java/com/example/jinfang/easiercollege/Splash.java
@@ -1,5 +1,6 @@
 package com.example.jinfang.easiercollege;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -9,7 +10,7 @@ import android.view.MenuItem;
 import android.os.Handler;
 
 
-public class Splash extends ActionBarActivity {
+public class Splash extends Activity{
 
     /** Duration of wait */
     private final int SPLASH_DISPLAY_LENGTH = 1000;
